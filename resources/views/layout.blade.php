950e93e367a7fd27c7d8acf968121f6baf03595a
Thinh Nguyen <tienthinh12102>
2021-06-10 11:22:26 +0700

    Create Laravel

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Truyện Tranh 247</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+       
+    </head>
+    <body >
+        <h1>Sách Truyện</h1>
+    </body>
+</html>
