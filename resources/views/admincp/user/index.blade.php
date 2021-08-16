ce42b0f58512b901d80be6b4b7d7bd49eae5218d
tienthinh12102 <tienthinh12102@gmail.com>
2021-08-16 09:12:43 +0700

    Spatie

---
+++ resources/views/admincp/user/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+@include('layouts.nav')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Liệt kê User</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <table class="table table-striped">
+                      <thead>
+                        <tr>
+                          <th scope="col">No.</th>
+                          <th scope="col">Tên User</th>
+                          <th scope="col">Slug thể loại</th>
+                          <th scope="col">Mô tả</th>
+                          <th scope="col">Kích hoạt</th>
+                          <th scope="col">Quản lý</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+
+                      </tbody>
+                    </table>
+
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\ChapterController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\TheloaiController;
 use App\Http\Controllers\SachController;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -41,3 +42,4 @@ Route::resource('/truyen', TruyenController::class);
 Route::resource('/chapter', ChapterController::class);
 Route::resource('/theloai', TheloaiController::class);
 Route::resource('/sach', SachController::class);
+Route::resource('/user', UserController::class);
